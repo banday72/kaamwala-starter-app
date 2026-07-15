aeb54b7ad15db216cbcbe3cc7458606123f60a9d
banday72 <ahmedalibanday76@gmnail.com>
2026-07-15 16:06:36 +0500

    remove unsupported UserButton prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
                   </SignUpButton>
                 </Show>
                 <Show when="signed-in">
-                  <UserButton afterSignOutUrl="/" />
+                  <UserButton />
                 </Show>
               </nav>
             </div>
